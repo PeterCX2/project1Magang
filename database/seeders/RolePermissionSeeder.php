80a9271b2bf940608c06c6548d5153d988eaf2c2
PeterCleon <petercleon.1234@gmail.com>
2026-01-21 17:20:42 +0700

    feat: new seeder for roles and permissions

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = Role::create(['name' => 'admin']);
+        $user  = Role::create(['name' => 'user']);
+        $filmCreator = Role::create(['name' => 'film creator']);
+
+        $permission = [
+            Permission::create(['name' => 'view user']),
+            Permission::create(['name' => 'create user']),
+            Permission::create(['name' => 'edit user']),
+            Permission::create(['name' => 'delete user']),
+            Permission::create(['name' => 'view film']),
+            Permission::create(['name' => 'edit film']),
+            Permission::create(['name' => 'create film']),
+            Permission::create(['name' => 'delete film']),
+            Permission::create(['name' => 'view category']),
+            Permission::create(['name' => 'create category']),
+            Permission::create(['name' => 'delete category']),
+        ];
+
+        $admin->givePermissionTo($permission);
+        $filmCreator->givePermissionTo([
+            'view film',
+            'create film',
+            'edit film',
+            'delete film',
+            'view category',
+        ]);
+    }
+}
