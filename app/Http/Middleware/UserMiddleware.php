ae5bdbcdd096f0c01b8b39cc13cb5e78a5d5d4cf
PeterCleon <petercleon.1234@gmail.com>
2026-01-21 17:27:43 +0700

    refactor: delete admin and user middleware

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -1,25 +1,29 @@
 <?php
 
-namespace App\Http\Middleware;
+// namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Auth;
+// use Closure;
+// use Illuminate\Http\Request;
+// use Symfony\Component\HttpFoundation\Response;
+// use Illuminate\Support\Facades\Auth;
 
-class UserMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {   
-        if(Auth::user()->role == 'user'){
-            return $next($request);
-        }
+// class UserMiddleware
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+//      */
+//     public function handle(Request $request, Closure $next): Response
+//     {   
+//         if (!Auth::check()) {
+//             return redirect()->route('login');
+//         }
 
-        return redirect('dashboard')->with('error', "You don't have user access.");
-    }
-}
+//         if(Auth::user()->hasRole('user')){
+//             return $next($request);
+//         }
+
+//         return redirect('dashboard')->with('error', "You don't have user access.");
+//     }
+// }
