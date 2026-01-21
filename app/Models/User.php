d27f42e3ba772dc4cc8d80be793a6e0493b60c1d
PeterCleon <petercleon.1234@gmail.com>
2026-01-21 17:25:46 +0700

    feat: add HasRoles

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,13 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 
 class User extends Authenticatable
 {
     
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -22,7 +23,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'role',
         'email',
         'password',
     ];
